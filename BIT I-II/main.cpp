ce57265acc75c9ade90b14aeb7153baa9acb1934
RonShrestha <147997233+RonShrestha@users.noreply.github.com>
2025-03-11 07:32:05 +0545

    Add files via upload

--- BIT I-II/main.cpp
+++ BIT I-II/main.cpp
@@ -2,6 +2,8 @@
 #include <raylib.h>
 #include <deque>
 #include <raymath.h>
+#include <cstring>
+#include <string>
 
 using namespace std;
 
@@ -38,6 +40,170 @@ bool EventTriggered(double interval)
     return false;
 }
 
+class Snake
+{
+public:
+    deque<Vector2> body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+    Vector2 direction = {1, 0};
+    bool addSegment = false;
+
+    void Draw()
+    {
+        for (unsigned int i = 0; i < body.size(); i++)
+        {
+            float x = body[i].x;
+            float y = body[i].y;
+            Rectangle segment = Rectangle{offset + x * cellSize, offset + y * cellSize, (float)cellSize, (float)cellSize};
+            DrawRectangleRounded(segment, 1, 6, black);
+        }
+    }
+
+    void Update()
+    {
+        body.push_front(Vector2Add(body[0], direction));
+        if (addSegment == true)
+        {
+            addSegment = false;
+        }
+        else
+        {
+            body.pop_back();
+        }
+    }
+
+    void Reset()
+    {
+        body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+        direction = {1, 0};
+    }
+};
+
+class Food
+{
+
+public:
+    Vector2 position;
+    Texture2D texture;
+
+    Food(deque<Vector2> snakeBody)
+    {
+        Image image = LoadImage("Graphics/food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+        position = GenerateRandomPos(snakeBody);
+    }
+
+    ~Food()
+    {
+        UnloadTexture(texture);
+    }
+
+    void Draw()
+    {
+        DrawTexture(texture, offset + position.x * cellSize, offset + position.y * cellSize, WHITE);
+    }
+
+    Vector2 GenerateRandomCell()
+    {
+        float x = GetRandomValue(0, cellCount - 1);
+        float y = GetRandomValue(0, cellCount - 1);
+        return Vector2{x, y};
+    }
+
+    Vector2 GenerateRandomPos(deque<Vector2> snakeBody)
+    {
+        Vector2 position = GenerateRandomCell();
+        while (ElementInDeque(position, snakeBody))
+        {
+            position = GenerateRandomCell();
+        }
+        return position;
+    }
+};
+
+class Game
+{
+public:
+    Snake snake = Snake();
+    Food food = Food(snake.body);
+    bool running = true;
+    int score = 0;
+    Sound eatSound;
+    Sound wallSound;
+
+    Game()
+    {
+        InitAudioDevice();
+        eatSound = LoadSound("Sounds/eat.mp3");
+        wallSound = LoadSound("Sounds/wall.mp3");
+    }
+
+    ~Game()
+    {
+        UnloadSound(eatSound);
+        UnloadSound(wallSound);
+        CloseAudioDevice();
+    }
+
+    void Draw()
+    {
+        food.Draw();
+        snake.Draw();
+    }
+
+    void Update()
+    {
+        if (running)
+        {
+            snake.Update();
+            CheckCollisionWithFood();
+            CheckCollisionWithEdges();
+            CheckCollisionWithTail();
+        }
+    }
+
+    void CheckCollisionWithFood()
+    {
+        if (Vector2Equals(snake.body[0], food.position))
+        {
+            food.position = food.GenerateRandomPos(snake.body);
+            snake.addSegment = true;
+            score++;
+            PlaySound(eatSound);
+        }
+    }
+
+    void CheckCollisionWithEdges()
+    {
+        if (snake.body[0].x == cellCount || snake.body[0].x == -1)
+        {
+            GameOver();
+        }
+        if (snake.body[0].y == cellCount || snake.body[0].y == -1)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        snake.Reset();
+        food.position = food.GenerateRandomPos(snake.body);
+        running = false;
+        score = 0;
+        PlaySound(wallSound);
+    }
+
+    void CheckCollisionWithTail()
+    {
+        deque<Vector2> headlessBody = snake.body;
+        headlessBody.pop_front();
+        if (ElementInDeque(snake.body[0], headlessBody))
+        {
+            GameOver();
+        }
+    }
+};
 
 int main()
 {
@@ -46,9 +212,47 @@ int main()
     SetTargetFPS(60);
 
     Game game = Game();
+   string username = ""; // Variable to store the username
+    char text[32] = "";
+    bool ra = true;
 
     while (WindowShouldClose() == false)
     {
+        if (ra) {
+            
+        if (IsKeyPressed(KEY_ENTER)) {
+            username = text; // Store the input text into username when Enter is pressed
+            memset(text, 0, sizeof(text)); // Clear the input buffer
+            ra = false;
+        }
+
+        // Capture keyboard input
+        for (int i = 0; i < 32; i++) {
+            if (IsKeyPressed(KEY_BACKSPACE)) {
+                if (text[i] != '\0') {
+                    text[i] = '\0'; // Remove last character
+                }
+            } else if (IsKeyPressed(KEY_SPACE)) {
+                text[i] = ' '; // Add space
+            } else if (IsKeyPressed(KEY_DELETE)) {
+                memset(text, 0, sizeof(text)); // Clear the input buffer
+            } else if (text[i] == '\0') {
+                int key = GetKeyPressed(); // Get the pressed key
+                if ((key >= 32) && (key <= 125)) { // Check for valid ASCII range
+                    text[i] = (char)key; // Add character to the input buffer
+                }
+            }
+        }
+        
+        BeginDrawing();
+        ClearBackground(white);
+        DrawText("Enter your username: ", 10, 10, 20, black);
+       
+        DrawText(text, 10, 40, 20, black);
+       EndDrawing();
+       
+        } else{
+    
         BeginDrawing();
 
         if (EventTriggered(0.2))
@@ -86,11 +290,13 @@ int main()
         ClearBackground(white);
         DrawRectangleLinesEx(Rectangle{(float)offset - 5, (float)offset - 5, (float)cellSize * cellCount + 10, (float)cellSize * cellCount + 10}, 5, black);
         DrawText(TextFormat("%i", game.score), offset - 5, offset + cellSize * cellCount + 10, 40, black);
-        DrawText("player", offset - 5, offset + cellSize * cellCount + 40, 40, black);
+        DrawText( (username.c_str()), offset - 5, offset + cellSize * cellCount + 40, 40, black);
         game.Draw();
 
         EndDrawing();
+        }
     }
+    
     CloseWindow();
-    return 0;
+    
 }
